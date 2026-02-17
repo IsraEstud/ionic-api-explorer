2c07f054f2b284e8dd141b9bbab9f6273a2a8d04
ChaeMendoza <israelmendoza656@outlook.com>
2026-02-17 14:42:10 -0500

    Config routes to navigate in application

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,36 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+
+const PokemonPage = () => import("../views/PokemonPage.vue");
+const JokesPage = () => import("../views/JokesPage.vue");
+const WeatherPage = () => import("../views/WeatherPage.vue");
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '',
-    redirect: '/folder/Inbox'
+    path: "",
+    redirect: "/folder/Inbox",
   },
   {
-    path: '/folder/:id',
-    component: () => import ('../views/FolderPage.vue')
-  }
-]
+    path: "/folder/:id",
+    component: () => import("../views/FolderPage.vue"),
+  },
+  {
+    path: "/pokemon",
+    component: PokemonPage,
+  },
+  {
+    path: "/jokes",
+    component: JokesPage,
+  },
+  {
+    path: "/weather",
+    component: WeatherPage,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
